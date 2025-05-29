104b3f5d75d286717f43c8067b7b0b07dcd67871
Drew Ballance <drew@tangotango.net>
2025-05-29 11:02:19 -0500

    feat: add elixir scaffold

---
+++ lib/ex_stream_client.ex
@@ -0,0 +1,18 @@
+defmodule ExStreamClient do
+  @moduledoc """
+  Documentation for `ExStreamClient`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExStreamClient.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
